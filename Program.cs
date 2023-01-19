3ccc4890afcfbe32fc3c4a5c2cf4dc1c6364cc2e
Elena <persik.popol@gmail.com>
2023-01-19 23:41:45 +0300

    Выполнила задание

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿void Array()
 {
     int rows = 3;
-    int columns = 4;
+    int columns = 5;
 
     object[,] symbols = new object[rows, columns];
 
     FillArray(symbols);
+    Console.WriteLine("Ваш массив");
     OutputArr(symbols);
+    Console.WriteLine("Новый массив");
+    NewArray(symbols);
 }
 
 void FillArray(object[,] symbols)
@@ -34,6 +37,21 @@ void OutputArr(object[,] symbols)
         for (int j = 0; j < columns; j++)
         {
             Console.Write(symbols[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void NewArray(object[,] symbols)
+{
+    Random rand = new Random();
+    int rows = symbols.GetLength(0);
+    int columns = rand.Next(0, 4);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int p = 0; p < columns; p++)
+        {
+            Console.Write(symbols[i, rand.Next(0, 4)] + "  ");
         }
         Console.WriteLine();
     }
