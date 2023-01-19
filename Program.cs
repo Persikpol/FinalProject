9c5004c5a552ca687aec93b2ef0b51dab085b305
Elena <persik.popol@gmail.com>
2023-01-19 23:11:04 +0300

    Создала метод для вывода массива

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
     object[,] symbols = new object[rows, columns];
 
     FillArray(symbols);
-    
+    OutputArr(symbols);
 }
 
 void FillArray(object[,] symbols)
@@ -15,16 +15,28 @@ void FillArray(object[,] symbols)
     int columns = symbols.GetLength(1);
     for (int i = 0; i < rows; i++)
     {
+        Console.WriteLine($"Строка № {i + 1}");
         for (int j = 0; j < columns; j++)
         {
             Console.WriteLine ("Enter symbols");
             symbols[i, j] = Console.ReadLine();
-            
-            
         }
-        
     }
     
+}
+
+void OutputArr(object[,] symbols)
+{
+    int rows = symbols.GetLength(0);
+    int columns = symbols.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(symbols[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
 }
 
 Array();
