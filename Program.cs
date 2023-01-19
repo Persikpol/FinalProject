c92a2f6bf5d3bee18f683023447b9b405c907a9e
Elena <persik.popol@gmail.com>
2023-01-19 22:59:14 +0300

    Заполнила массив

--- Program.cs
+++ Program.cs
@@ -1,9 +1,30 @@
-﻿Array()
+﻿void Array()
 {
     int rows = 3;
     int columns = 4;
 
-    int[,] symbols = new int[rows, columns];
+    object[,] symbols = new object[rows, columns];
+
+    FillArray(symbols);
+    
 }
 
+void FillArray(object[,] symbols)
+{
+    int rows = symbols.GetLength(0);
+    int columns = symbols.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.WriteLine ("Enter symbols");
+            symbols[i, j] = Console.ReadLine();
+            
+            
+        }
+        
+    }
+    
+}
 
+Array();
